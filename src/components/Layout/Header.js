39b969736bd91d69783d34fbc6f1baa0f6650ba7
Sabitjan Aiylchiev <a1yl4ievvsake@gmail.com>
2021-11-15 14:00:40 +0300

    hww44

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './Header.module.css'
+import mealsImage from '../../assets/meals.jpg';
+function Header() {
+    return (
+        <>
+        <header className={classes.header}>
+            <h1>React meals</h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage}/>
+        </div>
+        </>
+    )
+}
+export default Header
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function HeaderCartButton(props) {
+    return (
+        <button>
+            <span>
+                cartIcon
+            </span>
+            <span>
+                You Cart
+            </span>
+            <span>4</span>
+        </button>
+    )
+}
+
+export default HeaderCartButton
