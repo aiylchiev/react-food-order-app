e90177bb885544f24030190e31ce59b7f951178b
Sabitjan Aiylchiev <a1yl4ievvsake@gmail.com>
2022-01-20 08:42:17 +0300

    finished

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,53 +1,67 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import classes from './Available.module.css'
 import MealItem from './MealItem'
+const DATA = [
+    {
+        id: '1',
+        name: "burger sake",
+        description: 'Americsn. sd sake',
+        price: 12.23
+    },
+    {
+        id: '2',
+        name: "burger sdsd",
+        description: 'Americsn. sd bolgaria',
+        price: 12.23
+    }
+]
 
 const AvailableMeals = () => {
-    const [meals, setMeals] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [httpError, setHttpError] = useState(null);
+    // const [meals, setMeals] = useState([]);
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [httpError, setHttpError] = useState(null);
 
-    useEffect(() => {
-        const fetchMeals = async () => {
-            const response = await fetch('https://react-http-8fe77-default-rtdb.firebaseio.com/meals.json');
-            if (!response.ok) {
-                throw new Error('Something went wrong!');
-            };
-            const responseData = await response.json();
-            const loadedMeals = [];
-            for (const key in responseData) {
-                loadedMeals.push({
-                    id: key,
-                    name: responseData[key].name,
-                    description: responseData[key].description,
-                    price: responseData[key].price
-                })
-            }
+    // useEffect(() => {
+    //     const fetchMeals = async () => {
+    //         const response = await fetch('https://react-http-8fe77-default-rtdb.firebaseio.com/meals.json');
+    //         if (!response.ok) {
+    //             throw new Error('Something went wrong!');
+    //         };
+    //         const responseData = await response.json();
+    //         const loadedMeals = [];
+    //         for (const key in responseData) {
+    //             loadedMeals.push({
+    //                 id: key,
+    //                 name: responseData[key].name,
+    //                 description: responseData[key].description,
+    //                 price: responseData[key].price
+    //             })
+    //         }
 
-            setMeals(loadedMeals);
-            setIsLoading(false);
-        }
+    //         setMeals(loadedMeals);
+    //         setIsLoading(false);
+    //     }
 
-        fetchMeals().catch(error => {
-            setIsLoading(false);
-            setHttpError(error.message);
-        });
-    }, []);
+    //     fetchMeals().catch(error => {
+    //         setIsLoading(false);
+    //         setHttpError(error.message);
+    //     });
+    // }, []);
 
-    if (isLoading) {
-        return <section className={classes.MealsLoading}>
-            <p>Loading...</p>
-        </section>
-    }
+    // if (isLoading) {
+    //     return <section className={classes.MealsLoading}>
+    //         <p>Loading...</p>
+    //     </section>
+    // }
 
-    if (httpError) {
-        return <section className={classes.MealsError}>
-            <p>{httpError}</p>
-        </section>
-    }
+    // if (httpError) {
+    //     return <section className={classes.MealsError}>
+    //         <p>{httpError}</p>
+    //     </section>
+    // }
 
-    const mealsList = meals.map(meal => (
+    const mealsList = DATA.map(meal => (
         <MealItem
             id={meal.id}
             key={meal.id}
